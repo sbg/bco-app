61e2eb0d46f0d2c17315da0be38c60a180669c5c
Soner Koc <sonerkoc92@gmail.com>
2020-03-16 09:32:47 -0400

    update descriptions

--- ui/interface_variables.R
+++ ui/interface_variables.R
@@ -11,13 +11,13 @@
 include_ui_login_page = FALSE
 
 # Interface text
-ui_home_desc_1 = "The Genomics Compliance Suite provides tools for generating BioCompute Objects and tools for working with BioCompute Objects. The Genomics Compliance Suite can generate BioCompute Objects from user text input, workflows written in Common Workflow Language, and from task information generated on the Cancer Genomics Cloud. The Genomics Compliance Suite also include tppls for revievin and valdiating BioCompute Objects. This version of the Genomics Compliance Suite extends our initial prototype developed for the precisionFDA BioCompute Object Challenge."
-ui_home_desc_2 = "The BioCompute Object standards aims to faciliate the communication of Next Generation Sequencing Analyses by providing a structured way for communicating complex analyses as commonly seen in bioinformatics and precision medicine applications. See the BioCompute Object webpage for additional information."
+ui_home_desc_1 = "The Genomics Compliance Suite provides tools for generating BioCompute Objects and tools for working with BioCompute Objects. The Genomics Compliance Suite can generate BioCompute Objects from user text input, workflows written in Common Workflow Language (CWL), and from CWL/task information generated on the Cancer Genomics Cloud. The Genomics Compliance Suite also includes tools for reviewing and validating BioCompute Objects. This version of the Genomics Compliance Suite extends our initial prototype developed for the precisionFDA BioCompute Object Challenge."
+ui_home_desc_2 = "The BioCompute Object standard aims to facilitate the communication of Next Generation Sequencing Analyses by providing a structured way for communicating complex analyses as commonly seen in bioinformatics and precision medicine applications. See the BioCompute Object webpage for additional information (https://biocomputeobject.org/index.html)."
 
 # Tab titles
 title_home = "Home"
 title_txt_creator = "Text Composer"
-title_local_composer = "File Composer"
+title_local_composer = "CWL Composer"
 title_cgc_composer = "Platform Composer"
 title_browser = "Browser"
 title_valid = "Validator"
